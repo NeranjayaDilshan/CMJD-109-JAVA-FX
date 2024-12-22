661de3afc80b85b3c36a36c143be4354108445ad
NeranjayaDilshan <neranjayadilshan@gmail.com>
2024-12-22 15:58:27 +0530

    add

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL resource = getClass().getResource("Main.fxml");
+        Parent root = FXMLLoader.load(resource);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("This is Java FX");
+        primaryStage.show();
+    }
+
+}

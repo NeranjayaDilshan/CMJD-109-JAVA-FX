8965b96aa83e1cab612054c616d072efd03322a1
NeranjayaDilshan <neranjayadilshan@gmail.com>
2025-01-05 15:39:54 +0530

    config controller

--- src/App.java
+++ src/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL resource = getClass().getResource("Main.fxml");
+        URL resource = getClass().getResource("view/Customer.fxml");
         Parent root = FXMLLoader.load(resource);
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("This is Java FX");

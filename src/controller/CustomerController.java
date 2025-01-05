5ff98136e4f0c615b696602bac915bb3af548725
NeranjayaDilshan <neranjayadilshan@gmail.com>
2025-01-05 16:03:10 +0530

    btn action

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,34 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
 
+    @FXML
+    private TextArea txtAddress;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtTitle;
+
+     @FXML
+    void btnCustomerDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerSaveOnAction(ActionEvent event) {
+
+    }
 }

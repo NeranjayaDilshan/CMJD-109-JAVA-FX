77bcf286c4608dd65de7f8977c6f9ffa1e3b6b84
NeranjayaDilshan <neranjayadilshan@gmail.com>
2025-01-19 15:43:07 +0530

    new main ui

---
+++ src/controller/MainController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+    
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrdersOnAction(ActionEvent event) {
+
+    }
+}
